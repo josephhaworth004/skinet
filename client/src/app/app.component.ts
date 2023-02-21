8c25e0dfb8eb6be96b54a3d847354e2cf1d19899
josephhaworth004 <josephhaworth004@gmail.com>
2023-02-21 18:43:33 +0000

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './Models/Pagination';
+import { Product } from './Models/Product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+// onInitialization is another lifecycle hook
+// It's provided as an interface
+export class AppComponent implements OnInit{
   title = 'Skinet';
+  // Property to store what we get returned from api
+  products: Product[] = [];
+
+// Lifecycle hooks. Various stages an entity goes through
+// Inject something into constructor
+// Make it private so we can only use http in this class
+
+  constructor(private http: HttpClient) {}
+
+  // Normally considered too early to call this inside constructor
+  // Typically use constructor for dependency injection
+  ngOnInit(): void {
+    // Will return an observable of the response body as a js object
+    // Must subscribe to observable
+    // will auto-unsubscribe when complete
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?PageSize=50').subscribe({
+    //   <Pagination<Product[]>> allows us to use type safety with typescript in out methods. I.E. no need to use "any"
+    next: (response) => this.products = response.data, 
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+        console.log('Extra statement');
+      }
+    })
+  }
 }
+
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent, // We added this via Angular cli
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    // Add http client module
+    // to help with making an http request to the API and display it in html
+    // Needs an import above
+    // Implemented in app.component.ts
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
